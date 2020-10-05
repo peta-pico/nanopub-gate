8e3b71f99f1489baa4029e90ca1162ecc724727b
Tobias Kuhn <kuhntobias@gmail.com>
2020-10-05 10:18:28 +0200

    Push nanopubs to endpoint

--- src/main/java/org/petapico/nanopubgate/NanopubGateServlet.java
+++ src/main/java/org/petapico/nanopubgate/NanopubGateServlet.java
@@ -1,22 +1,30 @@
 package org.petapico.nanopubgate;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.repository.sparql.SPARQLRepository;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.Rio;
 import org.nanopub.Nanopub;
 import org.nanopub.NanopubImpl;
+import org.nanopub.NanopubUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NanopubGateServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 0L;
+
+	private SPARQLRepository repository;
+	private String[] uriPatterns;
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -30,7 +38,21 @@ public class NanopubGateServlet extends HttpServlet {
 			resp.sendError(400, "Error reading nanopub: " + ex.getMessage());
 		}
 		if (np != null) {
-			// TODO
+			boolean include = false;
+			List<Statement> statements = new ArrayList<>();
+			for (Statement st : NanopubUtils.getStatements(np)) {
+				if (!include) {
+					for (String p : uriPatterns) { 
+						if (st.getSubject().stringValue().equals(p)) include = true;
+						if (st.getPredicate().stringValue().equals(p)) include = true;
+						if (st.getObject().stringValue().equals(p)) include = true;
+					}
+				}
+				statements.add(st);
+			}
+			if (include) {
+				getRepository().getConnection().add(statements);
+			}
 		}
 	}
 
@@ -43,11 +65,23 @@ public class NanopubGateServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		logger.info("Init");
-		// Code to be run at start-up can go here
+		uriPatterns = System.getenv("NPG_URI_PATTERN").trim().split(" ");
 	}
 
 	private void setGeneralHeaders(HttpServletResponse resp) {
 		resp.setHeader("Access-Control-Allow-Origin", "*");
+	}
+
+	private SPARQLRepository getRepository() {
+		if (repository == null || !repository.isInitialized()) {
+			repository = new SPARQLRepository(System.getenv("NPG_ENDPOINT_URL"));
+			String username = System.getenv("NPG_ENDPOINT_USERNAME");
+			String password = System.getenv("NPG_ENDPOINT_PASSWORD");
+			if (username != null && password != null && !username.isEmpty() && !password.isEmpty())
+				repository.setUsernameAndPassword(username, password);
+			repository.initialize();
+		}
+		return repository;
 	}
 
 }
